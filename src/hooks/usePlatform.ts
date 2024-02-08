ae6f9a86f4bd3c5073a3cedd3d740d92d6b7e239
Kris Gospodinov <kris060601@gmail.com>
2024-02-08 20:21:18 +0200

    Custom hook for fetching single genre

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -2,8 +2,7 @@ import usePlatforms from "./usePlatforms";
 
 const usePlatform = (id?: number) => {
   const { data: platforms } = usePlatforms();
-  const platform = platforms?.results.find((p) => p.id === id);
-  return platform;
+  return platforms?.results.find((p) => p.id === id);
 };
 
 export default usePlatform;
